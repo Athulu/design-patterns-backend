67f6d684e7027c2be1a53501905088e3e04ef0c9
Nikodem Wójciak <nikodem1703@gmail.com>
2022-11-09 16:20:04 +0100

    Rozpoczęcie projektu

---
+++ src/main/java/designpatternsbackend/DesignPatternsBackendApplication.java
@@ -0,0 +1,13 @@
+package designpatternsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignPatternsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesignPatternsBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/designpatternsbackend/DesignPatternsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package designpatternsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignPatternsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
